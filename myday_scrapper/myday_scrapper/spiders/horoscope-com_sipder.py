51f625a60d0f58b4cd5493536bd72580178b254b
Dima Shulga <shudima@gmail.com>
2015-01-25 23:16:21 +0200

    libra data

--- myday_scrapper/myday_scrapper/spiders/horoscope-com_sipder.py
+++ myday_scrapper/myday_scrapper/spiders/horoscope-com_sipder.py
@@ -1,4 +1,6 @@
 import scrapy
+from textblob import TextBlob
+
 
 class HoroscopeComSpider(scrapy.Spider):
 	name = "HoroscopeCom"
@@ -8,4 +10,24 @@ class HoroscopeComSpider(scrapy.Spider):
 
 
 	def parse(self, response):
-		print('Response')
+		for sel in response.xpath('//div[@id="textline"]'):
+			horoscopeText = sel.xpath('text()').extract()
+
+			text = ''.join(horoscopeText)
+			blob = TextBlob(text)
+
+
+			polarity_sum = 0.0;
+			count = 0;
+
+			for sentence in blob.sentences:
+				polarity_sum = polarity_sum + sentence.sentiment.polarity
+				count = count + 1;
+
+
+			polarity_avg = polarity_sum / count
+
+			print(horoscopeText)
+			print(polarity_avg)
+			print(blob.noun_phrases)
+			
